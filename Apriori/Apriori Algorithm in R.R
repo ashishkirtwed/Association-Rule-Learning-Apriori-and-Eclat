6fc0f7486c253138271fa46d2c69b53c6b67213f
ashishkirtwed <=>
2021-06-28 17:46:34 +0530

    Adds vishualization code snippets

--- Apriori/Apriori Algorithm in R.R
+++ Apriori/Apriori Algorithm in R.R
@@ -9,5 +9,10 @@ dataset = read.csv('Market_Basket_Optimisation.csv',header = FALSE)
 dataset = read.transactions('Market_Basket_Optimisation.csv',sep=','
                             ,rm.duplicates = TRUE)
 
+#Getting the summary of the dataset 
+summary(dataset)
 
+itemFrequencyPlot(dataset,topN=50)
 
+#Training Apriori on the dataset
+rules = apriori(data=dataset,parameter=list(support=0.003,confidence=0.8))
